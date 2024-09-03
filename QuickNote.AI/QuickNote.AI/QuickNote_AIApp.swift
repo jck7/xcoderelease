a8ee3cb946edcf71b856fc5b8fcca46e29c96012
jck7 <j7kbball@gmail.com>
2024-09-03 10:51:09 -0400

    Initial commit

---
+++ QuickNote.AI/QuickNote.AI/QuickNote_AIApp.swift
@@ -0,0 +1,32 @@
+//
+//  QuickNote_AIApp.swift
+//  QuickNote.AI
+//
+//  Created by Admin on 9/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuickNote_AIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
